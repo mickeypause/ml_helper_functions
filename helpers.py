22f7118efa6b19091ac2e0d46fd003bef7f37698
mickeypause <serhii.pause@gmail.com>
2023-06-14 22:21:38 +0300

    add: set_seed function

--- helpers.py
+++ helpers.py
@@ -6,3 +6,14 @@ def accuracy_fn(y_true, y_pred):
     correct = torch.eq(y_true, y_pred).sum().item()
     acc = (correct / len(y_pred)) * 100
     return acc
+
+
+def set_seeds(seed: int = 42):
+    """Sets random sets for torch operations.
+    Args:
+        seed (int, optional): Random seed to set. Defaults to 42.
+    """
+    # Set the seed for general torch operations
+    torch.manual_seed(seed)
+    # Set the seed for CUDA torch operations (ones that happen on the GPU)
+    torch.cuda.manual_seed(seed)
